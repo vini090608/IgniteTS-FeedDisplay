bf3e8dce5f54f15ee2783f7a85ca77b367259a84
Vinicius Pereira <viniciuspereiraassuncao@gmail.com>
2026-03-03 13:40:48 -0300

    [UPDATE] Changing the interface of avatar to recieve all the props

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,19 +1,15 @@
+import { type ImgHTMLAttributes } from "react";
 import style from "./Avatar.module.css";
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
-  src: string;
-  alt?: string;
 }
 
-export function Avatar(props: AvatarProps) {
-  const hasBorder = props.hasBorder !== false;
-
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
   return (
     <img
       className={hasBorder ? style.avatarWithBorder : style.avatar}
-      src={props.src}
-      alt=""
+      {...props}
     />
   );
 }
